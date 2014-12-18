e30c832130e99a584345de62da047526b196deea
Hui Zhang <hui.zhang@oregonstate.edu>
2014-12-17 16:26:53 -0800

    initial commit

--- app/services/vocabulary_finder.rb
+++ app/services/vocabulary_finder.rb
@@ -1,3 +1,16 @@
 class VocabularyFinder
+  def call
+   ActiveTriples::Repositories.repositories[:Vocabulary].sparql.query("select ?resource where {
+ 	rdf:type ?rdf_type
+	filter strstarts(str(?resource), $base_uri)
+	}").dump
+  end
 
+  def base_uri
+   return Vocabulary.base_uri
+  end
+
+  def rdf_type
+   return "http://purl.org/dc/dcam/VocabularyEncodingScheme"
+  end
 end
