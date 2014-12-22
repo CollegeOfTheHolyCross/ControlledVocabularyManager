72bd8b11c23ac58fe70014370ace7c4ad8b09975
Hui <hui.zhang@oregonstate.edu>
2014-12-22 17:49:09 +0000

    new sparql query

--- app/services/vocabulary_finder.rb
+++ app/services/vocabulary_finder.rb
@@ -1,16 +1,15 @@
 class VocabularyFinder
   def self.call
-    ActiveTriples::Repositories.repositories[Vocabulary.repository].query_client.query("select ?resource where {
- 	rdf:type ?rdf_type
-	filter strstarts(str(?resource), $base_uri)
-	}").dump
+    ActiveTriples::Repositories.repositories[Vocabulary.repository].query_client.query("select distinct ?resource where {
+	?resource rdf:type $type
+	filter strstarts(str(?resource), $base_uri)}")
   end
 
   def base_uri
    return Vocabulary.base_uri
   end
 
-  def rdf_type
+  def type
    return "http://purl.org/dc/dcam/VocabularyEncodingScheme"
   end
 end
