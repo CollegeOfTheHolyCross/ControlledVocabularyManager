dc27e65b8b23b97b26e3e9f123c8118f0a644e99
Linda Sato <lsato@uoregon.edu>
2015-02-23 10:10:03 -0800

    brought over login code from prev version

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -1,6 +1,6 @@
 class VocabulariesController < ApplicationController
   before_filter :load_vocab, :only => :show
-
+  before_filter :authorize, :only => [:new, :create]
   def index
   end
 
@@ -11,4 +11,26 @@ class VocabulariesController < ApplicationController
   def create
     VocabularyCreator.call(params[:vocabulary], CreateResponder.new(self))
   end
+  
+   def authorize
+    if session[:authorized] != true
+      flash[:notice] = "Not authorized for this page."
+      redirect_to '/'
+  	
+  	end
+  end
+
+  class CreateResponder < SimpleDelegator
+    def success(vocabulary)
+      redirect_to term_path(vocabulary)
+    end
+
+    def failure(vocabulary)
+      __getobj__.instance_variable_set(:@vocabulary, vocabulary)
+      render :new
+    end
+    
+   
+  end
+
 end
